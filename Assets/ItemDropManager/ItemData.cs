93839038dfc552c02fe4f9e4b2a0edb3a8d09075
JoãoPedroSantosC <dan7santos@gmail.com>
2023-10-19 20:10:03 -0300

    Added ItemDropManager

---
+++ Assets/ItemDropManager/ItemData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Data", menuName = "Item Data")]
+public class ItemData : ScriptableObject
+{
+    public GameObject prefab;
+    int value;
+    float weight;
+    //etc...
+}
