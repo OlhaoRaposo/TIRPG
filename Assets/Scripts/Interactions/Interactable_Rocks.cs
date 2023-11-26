df9132d5e936faa07c78dd787cdd33270485e645
Gabriel Viana <gabrielvianafonseca1590@gmail.com>
2023-11-26 15:33:05 -0300

    Interactble rocks and item
    Scripts from Gabriel's level

---
+++ Assets/Scripts/Interactions/Interactable_Rocks.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_Rocks : MonoBehaviour, IInteractable
+{
+    [SerializeField] Transform galonPosition;
+    [SerializeField] GameObject particles;
+    Transform item;
+   public void Interact(PlayerInteractions player)
+    {
+        item = player.getItem();
+        item.SetParent(galonPosition);
+        item.localPosition = Vector3.zero;
+
+        Invoke("DestroyRocks", 5f);
+    }
+
+    public void DestroyRocks()
+    {
+        item.gameObject.SetActive(false);
+        gameObject.SetActive(false);
+        Instantiate(particles, transform.position, Quaternion.identity);
+    }
+}
