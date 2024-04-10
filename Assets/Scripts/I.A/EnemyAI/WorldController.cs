7c85508078c1a0b21f5e91f36a627c3fe17c6644
Daniel Souza <DanielDomingoss@hotmail.com>
2024-04-10 09:01:24 -0300

    Shader

--- Assets/Scripts/I.A/EnemyAI/WorldController.cs
+++ Assets/Scripts/I.A/EnemyAI/WorldController.cs
@@ -21,7 +21,8 @@ public class WorldController : MonoBehaviour
     private GameObject essentialsCanvas;
     
     [Header("Timer Settings")]
-    private DateTime currentHour; 
+    public DateTime currentHour;
+    public float blend;
     [SerializeField] private float timeMultiplier;
     [SerializeField] private TextMeshProUGUI timeText;
     
@@ -30,6 +31,7 @@ public class WorldController : MonoBehaviour
     private SunLight sunLight;
     private List<GameObject> lightsObjects = new List<GameObject>();
     private Volume dayPostProcessing, nightPostProcessing;
+    [SerializeField] Material skyBoxMaterial;
 
     private void Start()
     {
@@ -81,6 +83,7 @@ public class WorldController : MonoBehaviour
         UpdateTimeOfDay();
         UpdateLightSettings();
         RotateSun();
+        GetTimeOfDayValue();
     }
     private void UpdateTimeOfDay() {
         currentHour = currentHour.AddSeconds(Time.deltaTime * timeMultiplier);
@@ -117,19 +120,34 @@ public class WorldController : MonoBehaviour
         }
         sunLight.sunLight.transform.rotation = Quaternion.AngleAxis(sunLightRotation,Vector3.right);
     }
+
+    public void GetTimeOfDayValue() {
+        //Get the diference between sunris and sunset in seconds
+        TimeSpan sunriseToSunsetDuration = CalculateTimeDiference(sunLight.sunriseTime, sunLight.sunsetTime);
+        TimeSpan FromNightToMorning = CalculateTimeDiference(new TimeSpan(0,4,0,0), new TimeSpan(0,6,0,0));
+        //Lerp the blend value between 0 and .75 based on sunrise to sunset
+        if (currentHour.TimeOfDay > sunLight.sunriseTime && currentHour.TimeOfDay < sunLight.sunsetTime) {
+            TimeSpan timeSinceSunrise = CalculateTimeDiference(sunLight.sunriseTime, currentHour.TimeOfDay);
+            blend = Mathf.Lerp(0, .75f, (float)(timeSinceSunrise.TotalSeconds / sunriseToSunsetDuration.TotalSeconds));
+
+        }else if (currentHour.TimeOfDay > new TimeSpan(0, 4, 40, 0) && currentHour.TimeOfDay < new TimeSpan(0, 5, 40, 0)) {
+            TimeSpan timeSinceSunrise = CalculateTimeDiference(new TimeSpan(0, 4, 0, 0), currentHour.TimeOfDay);
+            blend = Mathf.Lerp(.75f, 1, (float)(timeSinceSunrise.TotalSeconds / FromNightToMorning.TotalSeconds));
+        }
+    }
+
     private void UpdateLightSettings()
     {
         float dotProduct = Vector3.Dot(sunLight.sunLight.transform.forward, Vector3.down);
         sunLight.sunLight.intensity = Mathf.Lerp(0, sunLight.maxSunLightIntesity, sunLight.lightAnimation.lightChangeCurve.Evaluate(dotProduct));
         sunLight.moonLight.intensity = Mathf.Lerp(sunLight.maxMoonLightIntesity, 0, sunLight.lightAnimation.lightChangeCurve.Evaluate(dotProduct));
-        RenderSettings.ambientLight = Color.Lerp(sunLight.lightAnimation.nightAmbientLight, sunLight.lightAnimation.dayAmbientLight, sunLight.lightAnimation.lightChangeCurve.Evaluate(dotProduct));
+        //changes the ambient light to the min in start hour and max in mid sunset hour
+        RenderSettings.ambientLight = Color.Lerp(sunLight.lightAnimation.dayAmbientLight, sunLight.lightAnimation.nightAmbientLight, sunLight.lightAnimation.lightChangeCurve.Evaluate(dotProduct));
         //changes the ambient intensity to the min in start hour and max in mid sunset hour
-        RenderSettings.ambientIntensity = Mathf.Lerp(.32f, 2.74f, sunLight.lightAnimation.lightChangeCurve.Evaluate(dotProduct));
+        RenderSettings.ambientIntensity = Mathf.Lerp(.32f, 1f, sunLight.lightAnimation.lightChangeCurve.Evaluate(dotProduct));
         //lerp the volume of the day post processing to night post proccessing between day and night
-        if(dayPostProcessing !=null)
-            dayPostProcessing.weight = Mathf.Lerp(1, 0, sunLight.lightAnimation.lightChangeCurve.Evaluate(dotProduct));
-        if(nightPostProcessing !=null)
-            nightPostProcessing.weight = Mathf.Lerp(0, 1, sunLight.lightAnimation.lightChangeCurve.Evaluate(dotProduct));
+        // if current hour is between 6 and midDay blend is lerping from 0 to 0.25 else if current hour is between midDay and 17 blend is lerping from 0.25 to .5 else if current hour is between 17 and 4 blend is lerping from .5 to .75 else if current hour is between 4 and 6 blend is lerping from .75 to 1
+        skyBoxMaterial.SetFloat("_Blend", blend);
     }
     private TimeSpan CalculateTimeDiference(TimeSpan fromTime, TimeSpan toTime)
     {
