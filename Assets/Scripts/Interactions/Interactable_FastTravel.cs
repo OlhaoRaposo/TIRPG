213f429a04ec722c461b727b2175a021ff9363a7
Daniel Souza <DanielDomingoss@hotmail.com>
2024-06-11 04:50:40 -0300

    nsei

--- Assets/Scripts/Interactions/Interactable_FastTravel.cs
+++ Assets/Scripts/Interactions/Interactable_FastTravel.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Interactable_FastTravel : MonoBehaviour, IInteractable
 {
     public bool isTutorial = false;
+    public bool isBossRoom = false;
     public void Interact(PlayerInteractions player)
     {
         if (isTutorial) {
             PlayerMovement.instance.TeleportPlayer(new Vector3(700,64,170));
+            return;
+        }
+        if (isBossRoom) {
+            PlayerMovement.instance.TeleportPlayer(new Vector3(322.545f,82.2154f,-760.2823f));
             return;
         }
         UIManager.instance.ToggleFastTravelMenu();
